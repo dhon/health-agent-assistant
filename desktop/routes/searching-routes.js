61cda6324b756fc2a5e4b2530877411d82d218d3
Adam <adam@playnimbus.com>
2015-12-06 20:20:51 -0500

    A start for the results page. Added more styling and formatting.

--- desktop/routes/searching-routes.js
+++ desktop/routes/searching-routes.js
@@ -11,8 +11,25 @@ router.post('/', function(req, res) {
 		console.log(attribute+": "+data[attribute]);
 	}
 	
+	// TODO: use the form to search the DB and get results
+	
+	var results = {
+		restaurants: [
+			{name: "Joe's Pizza", location:"123 Rd, Leverett MA", inspections:[
+				{date:'11/19/2015', violations:[
+					{code:"3.2.12", description:"Eggs above acceptable temperature."},
+					{code:"6.3.1", description:"Residue build up on bottom of fryer."}
+				]},
+				{date:'11/28/2014'},
+				{date:'11/12/2013'}
+			]},
+			{name: "Sal's Fish", location:"456 Rd, Leverett MA"},
+			{name: "Ben's Burritos", location:"789 Rd, Leverett MA"}
+		]
+	};
+	
 	// Test 
-	res.render('results', {data:data.restName});
+	res.render('results', results);
 });
 
 // Example route
