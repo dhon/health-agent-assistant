3370043e1260cb7d59d545282e2a8eb8a08f401d
James D <jfdesjar@umass.edu>
2015-12-08 21:34:45 -0500

    modified one of the data drop down menus

--- desktop/routes/data-routes.js
+++ desktop/routes/data-routes.js
@@ -7,10 +7,24 @@ var router = express.Router();
 // Example post
 router.post('/post', (req, res) => {console.log("posted");});
 
-// Example route
+// Route via search results
+router.post('/edit', function(req, res){
+	var primarykey = req.body.key;
+	var form; // = query db
+
+});
+
+// Main route
 router.get('/', function(req, res) {
+
+	var restaurants = [{"test":"WcDonalds", "id":"123"}, {"test":"Dunkin Donuts", "id":"456"}];
+	var tanks;
+	var wells;
+
 	console.log("routed");
-	res.render("data_entry");
+	res.render("data_entry",{
+		list:restaurants
+	});
 });
 
 module.exports = router;
