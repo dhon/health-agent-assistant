b08fc08a47c3aad11bea8c39e82359362f61a445
Khalory <tk100794@gmail.com>
2015-12-10 21:33:43 +0000

    Federation: basic test api setup

--- federation/test/test.js
+++ federation/test/test.js
@@ -12,13 +12,40 @@ describe('User login', function() {
   });
 });
 
+var api_add1 = {
+	"location": ["Leverett"],
+	"type": "Restaurant",
+	"Name": "WcDonalds",
+	"Address": "999 Lois Lane",
+	"Telephone": "1113335555",
+	"Owner": "Bobby Malone",
+	"PIC": "Frederick Malone"
+};
+var api_add2 = {
+	"location": ["Leverett"],
+	"type": "Restaurant",
+	"Name": "McDonalds",
+	"Address": "The Street",
+	"Telephone": "9997874014",
+	"Owner": "Calvin",
+	"PIC": "Susie Derkins"
+};
+
 describe('Database add', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+  describe('addition', function () {
+    it('Should not get any errors', function () {
+      var query = sqlQuery.writeSQLAdd(api_add1);
+			db.run(api_add1.location, query, function callback(errors) {
+				errors.forEach(function(error) {
+					console.log(error);
+				});
+				assert.equal(0, errors.length);
+			});
     });
   });
+	describe('retrieval', function() {
+
+	});
 });
 
 describe('Database gets', function() {
