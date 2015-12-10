4ea0afd3c25afeb07a703c490ce55852588fe6f6
Jared Kaczynski <jpkaczynski@umass.edu>
2015-12-10 22:05:49 +0000

    Adding tests

--- federation/test/test.js
+++ federation/test/test.js
@@ -47,15 +47,77 @@ describe('Database add', function() {
 
 	});
 });
+//Testing /api/get
+//One record matches info provided
 
-describe('Database gets', function() {
+var api_get0 = {
+	"location": ["Leverett"],
+	"type": "Restaurant",
+	"Telephone":"1113335555",
+	"Owner":"Bobby Malone"
+};
+//Multiple records match info provided
+var api_get1 = {
+	"location": ["Leverett"],
+	"type": "Restaurant",
+};
+//No records match info provided
+var api_get2 = {
+	"location": ["Leverett"],
+	"type": "Restaurant",
+	"Telephone":"15845453535",
+};
+
+describe('Database (all) gets', function() {
+  describe('one record', function () {
+    it('should return an array.length>=1 when the value is present', function () {
+		var query = sqlQuery.writeSQLAdd(api_add1);
+			db.run(api_get0.location, query, function callback(errors) {
+				errors.forEach(function(error) {
+					console.log(error);
+				});
+				assert.equal(true, errors.length==1);
+			});
+    });
+  });
+});
+
+describe('Database (all) gets', function() {
+  describe('multiple records', function () {
+    it('should return >1 when there are multple results', function () {
+		var query = sqlQuery.writeSQLAdd(api_add1);
+			db.run(api_get0.location, query, function callback(errors) {
+				errors.forEach(function(error) {
+					console.log(error);
+				});
+				assert.equal(true, errors.length>1);
+			});
+    });
+  });
+});
+
+describe('Database (all) gets', function() {
+  describe('zero records', function () {
+    it('should return length 0 when there are zero results', function () {
+		var query = sqlQuery.writeSQLAdd(api_add1);
+			db.run(api_get0.location, query, function callback(errors) {
+				errors.forEach(function(error) {
+					console.log(error);
+				});
+				assert.equal(0, errors.length);
+			});
+    });
+  });
+});
+
+/*describe('Database gets', function() {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
     });
   });
-});
+});*/
 
 describe('Database edit', function() {
   describe('#indexOf()', function () {
