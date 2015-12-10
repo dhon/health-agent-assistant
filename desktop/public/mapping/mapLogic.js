5013514b856f215b1cfef74368a02a2ee1818cad
Noobulater <AmanBentley@gmail.com>
2015-12-10 18:38:14 -0500

    Added Dynamic Pin management

--- desktop/public/mapping/mapLogic.js
+++ desktop/public/mapping/mapLogic.js
@@ -1,6 +1,65 @@
 var pins = {};
+var map;
+
+function initializeMap() {
+  // this just creates the map object. The size is still specified in the map.handlebars
+  L.mapbox.accessToken = 'pk.eyJ1IjoiYXNoYWJseWdpbiIsImEiOiJjaWhwM2R4YWEwMWhwdDZtNGxuMXVobjYwIn0.wKvqtULva4f_BiM5KgWZYQ';
+  map = L.mapbox.map('map', 'ashablygin.ocg5flkb').setView([42.474, -72.598], 13);
+}
 
 function addPin(key, data) {
+  data.innerHTML = 'Location : 50:40 (Lat:Lon) <br> Lot #3<br> Tank Status : <u style="color:red">Poor</u><br> Trending : <br> <div style="background-color: red; width: 100%; height : 15px"></div>';
 
-  pin[key] = data;
+  var featureLayer = L.mapbox.featureLayer({
+      // this feature is in the GeoJSON format: see geojson.org
+      // for the full specification
+      type: 'Feature',
+      geometry: {
+          type: 'Point',
+          // coordinates here are in longitude, latitude order because
+          // x, y is the standard for GeoJSON and many formats
+          coordinates: data.coords
+      },
+      properties: {
+          title: data.type + ' #' + key,
+          description: data.innerHTML, // cool we can inject in here
+          // one can customize markers by adding simplestyle properties
+          // https://www.mapbox.com/guides/an-open-platform/#simplestyle
+          'marker-size': 'large',
+          'marker-color': '#3366ff',
+          'marker-symbol': data.type
+      }
+  }).addTo(map);
+
+  pins[key] = featureLayer;
 }
+
+// deletes a pin
+function deletePin(key) {
+  map.removeLayer(pins[key]);
+  delete pins[key];
+}
+
+// clears the map of markers
+function clearMap() {
+  for (var cur in pins) {
+    deletePin(cur);
+  }
+}
+
+initializeMap();
+// testing to see if it actually clears it
+// "1" is a key
+//
+//
+//Example Data object
+// {
+// type : "water"
+//  they swapped the freaking coordinates ^, so friggin annoying
+//            longitude, latitude
+// coords : [-72.5170, 42.3670]
+// }
+
+addPin("1", {type : "water", coords : [-72.5170, 42.3670]});
+addPin("2", {type : "cafe", coords : [-72.5170, 42.3640]});
+//clearMap();
--- desktop/routes/mapping-routes.js
+++ desktop/routes/mapping-routes.js
@@ -17,4 +17,22 @@ router.get('/welldemo', function(req, res) {
 	res.render("welldemo");
 });
 
+// remove this, and replace any calls to this with the correct query codes
+function skeleton_fetch() { }
+
+// This message is recieved as soon as the map application loads up and is ready to go
+// It uses this to start retreiving data, then sends the data back to the client after
+// said data is retreived.
+function skeleton_mapInitialized(client) {
+	//Jake/Ben, we should start the querying process here.
+
+	var data = skeleton_fetch();
+
+	//TODO : use ajax to give
+	// note : client doesn't actaully mean anything right now
+
+	// something like client.sendData(data) needs to happen.
+}
+
+
 module.exports = router;
