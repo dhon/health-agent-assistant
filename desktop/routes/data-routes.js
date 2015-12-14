4bef56277025ec261c2cc84dab4774845a2dc8d5
James D <jfdesjar@umass.edu>
2015-12-13 19:54:08 -0500

    default tab can be changed via route file

--- desktop/routes/data-routes.js
+++ desktop/routes/data-routes.js
@@ -19,15 +19,27 @@ router.get('/edit', function(req, res){
 	//var key = req.body.key;
 	completed_form = {"id":"19", "inspector":"Steve"}; // query db using key
 
+	//we can compare the data from the query to determine the type of form it is
+	//then set that to be the "current" one and make the other two just "tab-link"
+	var healthtab = "tab-link current";
+	var watertab = "tab-link";
+	var septictab = "tab-link";
+
 	if (completed_form === ""){
 		console.log("no data to edit");
 		res.redirect('/data');
 	}
+
 	else{
 		console.log("routed for editing");
+		var changes = "Submit changes";
 		res.render("data_entry",{
 			list:restaurants,
-			formdata:completed_form
+			formdata:completed_form,
+			submit_string:changes,
+			healthtabclass:healthtab,
+			watertabclass:watertab,
+			septictabclass:septictab
 		});
 	}
 });
@@ -35,9 +47,18 @@ router.get('/edit', function(req, res){
 // Main route
 router.get('/', function(req, res) {
 
+	var healthtab = "tab-link current";
+	var watertab = "tab-link";
+	var septictab = "tab-link";
+	var changes = "Submit";
+
 	console.log("routed");
 	res.render("data_entry",{
-		list:restaurants
+		list:restaurants,
+		submit_string:changes,
+		healthtabclass:healthtab,
+		watertabclass:watertab,
+		septictabclass:septictab
 	});
 });
 
