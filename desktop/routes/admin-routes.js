c675b70ddf4a9c8d931ffa76d265b932bbd6bf40
nsrecc <noel.recchia@gmail.com>
2015-12-15 21:44:25 -0500

    pre commit before starting to mess around with admin page user data retrieval stuff

--- desktop/routes/admin-routes.js
+++ desktop/routes/admin-routes.js
@@ -1,11 +1,11 @@
 var express = require('express');
+var querystring = require('querystring');
+var http = require('http');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 // This creates an express "router" that allows us to separate
 // particular routes from the main application.
 var router = express.Router();
-
-// Example post
-router.post('/post', function(req, res) {console.log("posted");});
 
 router.get('/', function(req, res) {
 	console.log("routed");
