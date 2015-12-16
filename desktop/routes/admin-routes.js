cc78a9350e7d29d65a5aee41a448c7a8996e0d70
nsrecc <noel.recchia@gmail.com>
2015-12-16 15:25:22 -0500

    Added test table in admin-routes

--- desktop/routes/admin-routes.js
+++ desktop/routes/admin-routes.js
@@ -9,7 +9,17 @@ var router = express.Router();
 
 router.get('/', function(req, res) {
 	console.log("routed");
-	res.render('admin');
+
+	// Fake array for testing
+	var userArray2 = [
+    	{ id: 1, username: 'jack', passwordhash: '5ebe2294ecd0e0f08eab7690d2a6ee69', savedsearches: '72', privileges: '5' },
+  		{ id: 2, username: 'jill', passwordhash: '51075igaf89fafh98d8ghav8gh8av89g', savedsearches: '22', privileges: '1' },
+  	 	{ id: 3, username: 'ben', passwordhash: '0u09hjdh09hdhadhubuicbuiahdphahd', savedsearches: '3', privileges: '41' }
+	];
+
+	res.render('admin', {
+		users: userArray2
+	});
 });
 
 module.exports = router;
