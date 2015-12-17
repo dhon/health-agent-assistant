d9e50837e88324fb134abbc049c6d8e4265e4018
kschmeckpeper <kschmeckpeper@umass.edu>
2015-12-17 12:49:29 -0500

    Added account creation

--- desktop/routes/admin-routes.js
+++ desktop/routes/admin-routes.js
@@ -13,8 +13,25 @@ var isAuthenticated = function (req, res, next) {
   res.redirect('/login');
 }
 
+router.post('/register', isAuthenticated, function(req, res){
+	console.log("Creating new user");
+	var userInfo = req.body;
+	
+	var xmlHttp = new XMLHttpRequest();
+	
+	var query = JSON.stringify({username:userInfo.username, passwordhash:userInfo.password});
+	
+	xmlHttp.open( "POST", 'http://localhost:3000/api/user/register', false ); // false for synchronous request
+	xmlHttp.setRequestHeader('Content-Type', 'application/json');
+		
+	//console.log("Sending query:"+query);
+	//console.log();
+	xmlHttp.send( query );
+	
+	res.redirect('/admin');
+});
+
 router.get('/', isAuthenticated, function(req, res) {
-	console.log("routed");
 
 	// Fake array for testing
 	var userArray2 = [
@@ -23,18 +40,9 @@ router.get('/', isAuthenticated, function(req, res) {
   	 	{ ID: 3, USERNAME: 'ben', PASSWORDHASH: '0u09hjdh09hdhadhubuicbuiahdphahd', savedsearches: '3', privileges: '41' }
 	];
 
-	var userArray = {};
+	
 
-	var user = {
-		ID:"",
-		USERNAME:"",
-		PASSWORDHASH:""
-		// savedsearches:"",
-		// privileges:""
-	};
-
-	var userResults = getAllUsers(user);
-	userArray = userResults.data;
+	var userArray = getAllUsers().data;
 
 	res.render('admin', {
 		users: userArray // replace with userArray2 to test local array
@@ -43,7 +51,7 @@ router.get('/', isAuthenticated, function(req, res) {
 
 
 // Originally from searching-routes.js and remade for users retrieval from DB
-function getAllUsers(query)
+function getAllUsers()
 {
 	var userDBResults = {};
 	var xmlHttp = new XMLHttpRequest();
@@ -53,19 +61,19 @@ function getAllUsers(query)
 	xmlHttp.open( "POST", 'http://localhost:3000/api/get', false ); // false for synchronous request
 	xmlHttp.setRequestHeader('Content-Type', 'application/json');
 		
-	console.log("Sending query:"+query);
-	console.log();
+	//console.log("Sending query:"+query);
+	//console.log();
 	xmlHttp.send( query );
 		
 	if(xmlHttp.responseText)
 	{
 		var data = JSON.parse(xmlHttp.responseText);
 		
-		console.log("Starting returned data");
-		console.log(data);
-		console.log("Ending Returned data");
-		console.log(data.success);
-		console.log();
+		// console.log("Starting returned data");
+		// console.log(data);
+		// console.log("Ending Returned data");
+		// console.log(data.success);
+		// console.log();
 		
 		if(data.success){
 			if(data.rows == undefined){
